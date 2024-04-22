606c4ef3d3f41201f6d3a1570c387c1d2bdebde6
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2024-04-22 01:50:39 -0500

    diseno formulario

--- postApp/forms.py
+++ postApp/forms.py
@@ -5,30 +5,30 @@ class SendEmailForm(forms.Form):
     
     Nombre = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder':'Indroduce tu nombre',
-        'class':'form-control bg-transparent border-0',
+        'class':'form-control bg-light border-0',
         'type':'text'
     }))
     
     Apellido = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder':'Indroduce tu Apellido',
-        'class':'form-control bg-transparent border-0',
+        'class':'form-control bg-ligth border-0',
         'type':'text'
     }))
     
     Telefono = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder':'Indroduce tu Telefono',
-        'class':'form-control bg-transparent border-0',
+        'class':'form-control bg-light border-0',
         'type':'number'
     }))
     
     Correo = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder':'Indroduce tu Correo',
-        'class':'form-control bg-transparent border-0',
+        'class':'form-control bg-light border-0',
         'type':'email'
     }))
     
     Mensaje = forms.CharField(widget=forms.Textarea(attrs={
         'placeholder':'Indroduce tu Mensaje',
-        'class':'form-control bg-transparent border-0',
+        'class':'form-control bg-light border-0',
         'rows': 5
     }))
