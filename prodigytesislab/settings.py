13354e7b3b640d6d20f236933f6bae9a06e222ec
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2024-05-07 22:15:24 -0500

    listo

--- prodigytesislab/settings.py
+++ prodigytesislab/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k&%_$^#a)8k^sdk!rlmv4i9wzn3(w4$mk8ke@cg*$)_4x=ym8^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.29.17.57']
 
 
 # Application definition
