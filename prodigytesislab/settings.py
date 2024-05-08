98f4451bfe1879b709f292ef901c310334109995
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2024-05-07 22:30:52 -0500

    404 template

--- prodigytesislab/settings.py
+++ prodigytesislab/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k&%_$^#a)8k^sdk!rlmv4i9wzn3(w4$mk8ke@cg*$)_4x=ym8^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['34.29.17.57:80','34.29.17.57', '127.0.0.1']
 
